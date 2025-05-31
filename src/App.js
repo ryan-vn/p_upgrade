44ecb7b60968e49f23b64ac27d31933cb39dfde2
vincent <vincent.cy@foxmail.com>
2025-06-01 06:34:14 +0800

    Refactor App component to remove direct imports of profession and reputation data, moving them to the util module for better organization. This change enhances code maintainability and streamlines data management.

--- src/App.js
+++ src/App.js
@@ -1,35 +1,13 @@
 import react, { useEffect, useRef, useCallback } from 'react';
 import './App.scss';
 import ItemWithTip from './components/ItemWithTip';
-import engineeringData from './api/engineering.json';
-import enchantingData from './api/enchanting.json';
-import alchemyData from './api/alchemy.json';
-import tailoringData from './api/tailoring.json';
-import leatherworkingData from './api/leatherworking.json';
-import blacksmithingData from './api/blacksmithing.json';
-import jewelcraftingData from './api/jewelcrafting.json';
-import inscriptionData from './api/inscription.json';
-import cookingData from './api/cooking.json';
-
-import { getServerListOptions, professionListOptions, sideListOptions } from './util';
+import { getServerListOptions, professionListOptions, sideListOptions, professionsTypeArr, professionsDataArr, reputationMap } from './util';
 
 import Select from './components/Select';
 
 import Message from './components/Message';
 
 import { useProfessionState, ACTIONS } from './hooks/useProfessionState';
-
-// 工程学 附魔 炼金术  裁缝 制皮 锻造 珠宝加工 铭文
-const professionsTypeArr = ['工程学', '附魔', '炼金术', '裁缝', '制皮', '锻造', '珠宝加工', '铭文', '烹饪'];
-const professionsDataArr = [engineeringData, enchantingData, alchemyData, tailoringData, leatherworkingData, blacksmithingData, jewelcraftingData, inscriptionData, cookingData];
-
-const reputationMap = {
-  3: '中立',
-  4: '友善',
-  5: '尊敬',
-  6: '崇敬',
-  7: '崇拜',
-};
 
 let hasReStore = window.localStorage.getItem('hasReStore');
 
